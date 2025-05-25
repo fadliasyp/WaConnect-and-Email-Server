3814811da26f4de58ff97b58b17f511fd54df3aa
Tatya Hanum Pramudita <113042083+velottya@users.noreply.github.com>
2025-05-25 22:48:41 +0700

    Fix code

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: 'postgres',         
   host: 'localhost',        
   database: 'postgres',  
-  password: 'Megumine14_',  
+  password: '',  
   port: 5432,               
 });
 
